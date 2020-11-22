19dfd8d47b59a946bd46fcf93ed76af420fe3c5f
vAgafonov <mail@ohranka-gsm.ru>
2020-11-22 17:48:10 +0300

    build stm32

--- STM32/MDK-ARM/RTE/_WOLF/RTE_Components.h
+++ STM32/MDK-ARM/RTE/_WOLF/RTE_Components.h
@@ -1,21 +1,21 @@
-
-/*
- * Auto generated Run-Time-Environment Configuration File
- *      *** Do not modify ! ***
- *
- * Project: 'WOLF' 
- * Target:  'WOLF' 
- */
-
-#ifndef RTE_COMPONENTS_H
-#define RTE_COMPONENTS_H
-
-
-/*
- * Define the Device Header File: 
- */
-#define CMSIS_device_header "stm32h7xx.h"
-
-
-
-#endif /* RTE_COMPONENTS_H */
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'WOLF' 
+ * Target:  'WOLF' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32h7xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
